fed5f3b48074d77bc0806451bfdef40b35ee8e40
Murilo Polese <murilopolese@gmail.com>
2019-12-02 21:09:51 +0000

    Add blur to hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,35 +1,62 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles'
 import Palette from './palette.js'
 
 function Hero(props) {
-	const style = {
-		position: 'relative',
-		backgroundColor: `${props.bgcolor}`,
-		backgroundImage: `url('${props.bgimage}')`,
-		backgroundSize: 'cover',
-		backgroundPosition: 'center center'
-	}
-	const shadeStyle = {
-		position: 'absolute',
-		width: '100%',
-		height: '100%',
-		top: 0,
-		left: 0,
-		background: props.shadecolor ? props.shadecolor : 'none',
-		opacity: props.shadeopacity,
-		zIndex: 0,
-		display: props.shadecolor ? 'block' : 'none'
-	}
-	const contentStyle = {
-		width: '100%',
-		zIndex: 10,
-		position: 'relative'
-	}
+	const useStyles = makeStyles({
+		root: {
+			position: 'relative',
+			overflow: 'hidden'
+		},
+		bgImage: {
+			position: 'absolute',
+			width: '100%',
+			height: '100%',
+			backgroundColor: `${props.bgcolor}`,
+			backgroundImage: `url('${props.bgimage}')`,
+			backgroundSize: 'cover',
+			backgroundPosition: 'center center'
+		},
+		bgImageBlurred: {
+			position: 'absolute',
+			backgroundColor: `${props.bgcolor}`,
+			backgroundImage: `url('${props.bgimage}')`,
+			backgroundSize: 'cover',
+			backgroundPosition: 'center center',
+			width: 'calc(100% + 10px)',
+			height: 'calc(100% + 10px)',
+			top: -5,
+			down: -5,
+			left: -5,
+			right: -5,
+			filter: 'blur(5px)',
+		},
+		shade: {
+			position: 'absolute',
+			width: '100%',
+			height: '100%',
+			top: 0,
+			left: 0,
+			background: props.shadecolor ? props.shadecolor : 'none',
+			opacity: props.shadeopacity,
+			zIndex: 0,
+			display: props.shadecolor ? 'block' : 'none',
+		},
+		content: {
+			width: '100%',
+			zIndex: 10,
+			position: 'relative'
+		},
+	})
+	const styles = useStyles()
+	const bgStyle = props.blur ? styles.bgImageBlurred : styles.bgImage
+
 	return (
-		<div style={style}>
-			<div style={shadeStyle}></div>
-			<div style={contentStyle}>
+		<div className={styles.root}>
+			<div className={bgStyle}></div>
+			<div className={styles.shade}></div>
+			<div className={styles.content}>
 				{props.children}
 			</div>
 		</div>
@@ -37,6 +64,7 @@ function Hero(props) {
 }
 
 Hero.propTypes = {
+	blur: PropTypes.bool,
 	bgcolor: PropTypes.string,
 	bgimage: PropTypes.string,
 	shadecolor: PropTypes.string,
@@ -44,6 +72,7 @@ Hero.propTypes = {
 }
 
 Hero.defaultProps = {
+	blur: false,
 	bgcolor: Palette.white,
 	shadeopacity: 0.5
 }
