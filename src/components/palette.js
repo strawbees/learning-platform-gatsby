53080f7bec5d503baba9891be3284b3b31016286
Murilo Polese <murilopolese@gmail.com>
2019-12-04 15:33:33 +0100

    Update footer layout

--- src/components/palette.js
+++ src/components/palette.js
@@ -5,7 +5,7 @@ export default {
 	green: '#00A99D',
 	lightGrey: '#EBEBEB',
 	mediumGrey: '#C1C1C1',
-	darkGrey: '#525252',
-	black: '#303030',
+	darkGrey: '#4D4D4D',
+	black: '#272727',
 	pink: '#FF7381'
 }
--- src/templates/partials/layoutfooter.js
+++ src/templates/partials/layoutfooter.js
@@ -1,49 +1,38 @@
 import React from "react"
-import { Link } from "gatsby"
 import { Container, Grid, Box } from '@material-ui/core'
 import Palette from '../../components/palette.js'
 import Typography from '../../components/typography.js'
-import StrawbeesLogo from '../../images/learninglogo.svg'
+import StrawbeesLogo from '../../images/learninglogowhite.svg'
 
 function LayoutFooter() {
 	return (
-		<Box bgcolor={Palette.mediumGrey} p={2} pt={12} textAlign={{xs:'center', md:'left'}}>
+		<Box
+			py={8}
+			textAlign={{xs:'center', md:'left'}}
+			bgcolor={Palette.darkGrey}
+			color={Palette.white}
+			>
 			<Container>
 				<Grid container spacing={2} direction="row" justify="space-between">
-					<Grid item xs={12} md={4}>
-						<Box width="100%"><img style={{maxWidth: '100%'}} src={StrawbeesLogo} alt="Strawbees Logo"/></Box>
-					</Grid>
-					<Grid item xs={12} md={3}>
-						<Typography>
-							<div><strong>STRAWBEES EDUCATION</strong></div>
-							<div><Link to="/">About</Link></div>
-							<div><Link to="/">Community</Link></div>
-							<div><Link to="/">Professional Development</Link></div>
-							<div><Link to="/">Lesson Plan</Link></div>
-						</Typography>
-					</Grid>
-					<Grid item xs={12} md={2}>
-						<Typography>
-							<div><strong>SUPPORT</strong></div>
-							<div><Link to="/">Getting Started</Link></div>
-							<div><Link to="/">FAQ</Link></div>
-							<div><Link to="/">Contact us</Link></div>
-						</Typography>
-					</Grid>
-					<Grid item xs={12} md={2}>
-						<Typography>
-							<div><strong>RESOURCES</strong></div>
-							<div><Link to="/">Lesson Plans & Activities</Link></div>
-							<br />
-							<div><strong><Link to="/">SHOP</Link></strong></div>
-						</Typography>
+					<Grid item xs={12}>
+						<Box textAlign="center">
+						<p>
+							<img src={StrawbeesLogo} width="200" alt="Strawbees Logo" />
+						</p>
+						</Box>
 					</Grid>
 					<Grid item xs={12}>
-						<Box textAlign="center" pt={8}>
-							<Typography>
-								<p>Strawbees AB, Stenkolsgatan 1B, 417 07 Gothenburg, Sweden.</p>
-								<p>©2019 STRAWBEES. All rights reserved</p>
-							</Typography>
+						<Box textAlign="center">
+								<p>
+									<Typography>
+										Strawbees AB, Stenkolsgatan 1B, 417 07 Gothenburg, Sweden.
+									</Typography>
+								</p>
+								<p>
+									<Typography>
+										©2019 STRAWBEES. All rights reserved
+									</Typography>
+								</p>
 						</Box>
 					</Grid>
 				</Grid>
