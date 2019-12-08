ef19ee257cd6a5ca277c8d85b3e62967d845cb33
Murilo Polese <murilopolese@gmail.com>
2019-12-08 12:16:31 +0100

    Adding hack for different build environments

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,39 @@
+// XXX: Hack to build multiple environment variables
+let pathPrefix, siteUrl, label, trackingId, buildEnv
+// buildEnv = 'stage'
+
+switch (buildEnv) {
+	case 'stage':
+		trackingId = 'UA-69443341-4'
+		pathPrefix = ''
+		siteUrl = 'https://learning-stage.strawbees.com'
+		break;
+	case 'production':
+		trackingId = 'UA-69443341-2'
+		pathPrefix = ''
+		siteUrl = 'https://learning.strawbees.com'
+		break;
+	case 'github':
+		pathPrefix = '/learning-platform-gatsby'
+		siteUrl = 'https://murilopolese.github.io'
+		break;
+	case 'local':
+		pathPrefix = '/public'
+		siteUrl = 'http://localhost:8080'
+		break;
+	case 'development':
+	default:
+		pathPrefix = ''
+		siteUrl = 'http://localhost:8000'
+}
+
 module.exports = {
-	// pathPrefix: '/public',
-	// pathPrefix: '/learning-platform-gastby',
+	pathPrefix: pathPrefix,
   siteMetadata: {
     title: `Strawbees Learning Platform`,
-    description: `Strawbees: The future of education`,
+    description: `Strawbees Learning Platform is an oasis for innovative teachers who embrace hands-on learning with Strawbees.`,
     author: `@strawbees`,
+		siteUrl: siteUrl,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -23,6 +52,13 @@ module.exports = {
         stylesProvider: {
           injectFirst: true,
         },
+      },
+    },
+		{
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: trackingId,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
