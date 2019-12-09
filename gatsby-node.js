9f2248073e07bf2d870fbf5992c91088c8fad7c7
Murilo Polese <murilopolese@gmail.com>
2019-12-09 19:46:00 +0100

    Create browser redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,8 +18,68 @@ async function getContent(contentPath) {
 	})
 }
 
+const redirectBatch = [
+	{
+		f: `/post/build-the-platonic-solids/`,
+		t: `/activity/build-the-platonic-solids/`
+	},
+	{
+		f: `/post/city-building/`,
+		t: `/lesson-plan/city-building/`
+	},
+	{
+		f: `/post/build-a-truss-bridge/`,
+		t: `/activity/build-a-truss-bridge/`
+	},
+	{
+		f: `/post/create-a-robot-racer/`,
+		t: `/activity/create-a-robot-racer/`
+	},
+	{
+		f: `/post/robotic-olympics/`,
+		t: `/lesson-plan/robotic-olympics/`
+	},
+	{
+		f: `/post/build-a-stacking-tower/`,
+		t: `/activity/build-a-stacking-tower/`
+	},
+	{
+		f: `/post/geometric-world/`,
+		t: `/lesson-plan/geometric-world/`
+	},
+	{
+		f: `/post/fractals/`,
+		t: `/activity/construct-a-sierpinski-pyramid/`
+	},
+	{
+		f: `/post/construct-a-sierpinski-pyramid/`,
+		t: `/activity/construct-a-sierpinski-pyramid/`
+	},
+	{
+		f: `/post/make-a-mechanical-arm/`,
+		t: `/activity/make-a-mechanical-arm/`
+	},
+	{
+		f: `/post/build-a-mechanical-crane/`,
+		t: `/activity/build-a-mechanical-crane/`
+	},
+	{
+		f: `/post/build-a-catapult/`,
+		t: `/activity/build-a-catapult/`
+	},
+	{
+		f: `/post/build-a-mechanical-claw/`,
+		t: `/activity/build-a-mechanical-claw/`
+	},
+	{
+		f: `/page/about/`,
+		t: `/about/`
+	},
+]
+
 exports.createPages = async function(e) {
 	const createPage = e.actions.createPage
+	const createRedirect = e.actions.createRedirect
 	const activities = await getContent('activities')
 	const lessonPlans = await getContent('lesson-plans')
 	const explorations = await getContent('explorations')
@@ -47,6 +107,15 @@ exports.createPages = async function(e) {
 			path: post.path,
 			component: require.resolve('./src/templates/post.js'),
 			context: { post: post }
+		})
+	})
+
+	redirectBatch.forEach(function(redirect) {
+		createRedirect({
+			fromPath: redirect.f,
+			isPermanent: true,
+			redirectInBrowser: true,
+			toPath: redirect.t,
 		})
 	})
 
