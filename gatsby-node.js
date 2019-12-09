b26147164b059fca1d1bfe28709186faa1d8eda0
Murilo Polese <murilopolese@gmail.com>
2019-12-09 20:05:42 +0100

    Redirecting with and without trailing slashes

--- gatsby-node.js
+++ gatsby-node.js
@@ -20,60 +20,60 @@ async function getContent(contentPath) {
 
 const redirectBatch = [
 	{
-		f: `/post/build-the-platonic-solids/`,
-		t: `/activity/build-the-platonic-solids/`
+		f: `/post/build-the-platonic-solids`,
+		t: `/activity/build-the-platonic-solids`
 	},
 	{
-		f: `/post/city-building/`,
-		t: `/lesson-plan/city-building/`
+		f: `/post/city-building`,
+		t: `/lesson-plan/city-building`
 	},
 	{
-		f: `/post/build-a-truss-bridge/`,
-		t: `/activity/build-a-truss-bridge/`
+		f: `/post/build-a-truss-bridge`,
+		t: `/activity/build-a-truss-bridge`
 	},
 	{
-		f: `/post/create-a-robot-racer/`,
-		t: `/activity/create-a-robot-racer/`
+		f: `/post/create-a-robot-racer`,
+		t: `/activity/create-a-robot-racer`
 	},
 	{
-		f: `/post/robotic-olympics/`,
-		t: `/lesson-plan/robotic-olympics/`
+		f: `/post/robotic-olympics`,
+		t: `/lesson-plan/robotic-olympics`
 	},
 	{
-		f: `/post/build-a-stacking-tower/`,
-		t: `/activity/build-a-stacking-tower/`
+		f: `/post/build-a-stacking-tower`,
+		t: `/activity/build-a-stacking-tower`
 	},
 	{
-		f: `/post/geometric-world/`,
-		t: `/lesson-plan/geometric-world/`
+		f: `/post/geometric-world`,
+		t: `/lesson-plan/geometric-world`
 	},
 	{
-		f: `/post/fractals/`,
-		t: `/activity/construct-a-sierpinski-pyramid/`
+		f: `/post/fractals`,
+		t: `/activity/construct-a-sierpinski-pyramid`
 	},
 	{
-		f: `/post/construct-a-sierpinski-pyramid/`,
-		t: `/activity/construct-a-sierpinski-pyramid/`
+		f: `/post/construct-a-sierpinski-pyramid`,
+		t: `/activity/construct-a-sierpinski-pyramid`
 	},
 	{
-		f: `/post/make-a-mechanical-arm/`,
-		t: `/activity/make-a-mechanical-arm/`
+		f: `/post/make-a-mechanical-arm`,
+		t: `/activity/make-a-mechanical-arm`
 	},
 	{
-		f: `/post/build-a-mechanical-crane/`,
-		t: `/activity/build-a-mechanical-crane/`
+		f: `/post/build-a-mechanical-crane`,
+		t: `/activity/build-a-mechanical-crane`
 	},
 	{
-		f: `/post/build-a-catapult/`,
-		t: `/activity/build-a-catapult/`
+		f: `/post/build-a-catapult`,
+		t: `/activity/build-a-catapult`
 	},
 	{
-		f: `/post/build-a-mechanical-claw/`,
-		t: `/activity/build-a-mechanical-claw/`
+		f: `/post/build-a-mechanical-claw`,
+		t: `/activity/build-a-mechanical-claw`
 	},
 	{
-		f: `/page/about/`,
-		t: `/about/`
+		f: `/page/about`,
+		t: `/about`
 	},
 ]
 
@@ -111,12 +111,22 @@ exports.createPages = async function(e) {
 	})
 
 	redirectBatch.forEach(function(redirect) {
+		// Redirect without trailing slashes
 		createRedirect({
 			fromPath: redirect.f,
 			isPermanent: true,
 			redirectInBrowser: true,
 			toPath: redirect.t,
 		})
+		// Redirect with trailing slashes
+		if (redirect.f !== '/') {
+			createRedirect({
+				fromPath: `${redirect.f}/`,
+				isPermanent: true,
+				redirectInBrowser: true,
+				toPath: redirect.t,
+			})
+		}
 	})
 
 	return Promise.resolve()
