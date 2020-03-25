e950b26a2c0ce381c673cb0ddecacef96452a771
Murilo Polese <murilopolese@gmail.com>
2020-03-25 21:57:17 +0000

    Update front end models

--- build-src/models.js
+++ build-src/models.js
@@ -24,3 +24,34 @@ exports.getPost = function(data) {
 		content: data.content
 	}
 }
+exports.getMenu = function(menu) {
+	const menuModel = (m) => {
+		return {
+			id: m.db_id,
+			url: m.url,
+			label: m.title,
+			menuItems: null
+		}
+	}
+	let menuHash = menu.reduce(
+		(acc, m) => {
+			let parent = acc[m.menu_item_parent] || []
+			parent.push(menuModel(m))
+			acc[m.menu_item_parent] = parent
+			return acc
+		},
+		{}
+	)
+	let result = menuHash[0].map((m) => {
+		m.menuItems = menuHash[m.id]
+		return m
+	})
+	return result
+}
+exports.getCategory = function(category) {
+	return {
+		name: category.name,
+		path: category.path,
+		description: category.description
+	}
+}
